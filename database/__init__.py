de3b36ad91cb1708c064f44466065ed9398103c4
pocong <81405011+poocong@users.noreply.github.com>
2022-08-06 13:56:46 +0700

    [ + ] PociUserbot

---
+++ database/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2020-2021 by Toni880@Github, < https://github.com/Toni880 >.
+#
+# This file is part of < https://github.com/Toni880/Prime-Userbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Toni880/Prime-Userbot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import motor.motor_asyncio
+
+from config import MONGO_URI
+
+cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
