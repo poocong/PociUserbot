dcbd1fd842a02a947db4917fc9194d7a75063d21
pocong <81405011+poocong@users.noreply.github.com>
2022-09-01 23:08:58 +0700

    [ + ] missing import

--- PociUb/__main__.py
+++ PociUb/__main__.py
@@ -12,7 +12,7 @@ from uvloop import install
 
 from config import BOT_VER, CMD_HANDLER
 from PociUb import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
-from PociUb.helpers.misc import create_botlog, heroku
+from PociUb.helpers.misc import create_botlog, git, heroku
 
 MSG_ON = """
 🔥 **PociUserbot Berhasil Di Aktifkan**
