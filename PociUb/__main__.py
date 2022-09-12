23f36253f73922f647aa80f95c920adb04750b70
pocong <81405011+poocong@users.noreply.github.com>
2022-09-13 06:15:47 +0700

    [ + ] PociUb

--- PociUb/__main__.py
+++ PociUb/__main__.py
@@ -7,22 +7,28 @@
 #
 # ReC0de @pocongonlen
 
+import importlib
+
 from pyrogram import idle
 from uvloop import install
 
 from config import BOT_VER, CMD_HANDLER
 from PociUb import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
-from PociUb.helpers.misc import create_botlog, git, heroku
+from PociUb.helpers.misc import create_botlog, heroku
+from PociUb.modules import ALL_MODULES
 
 MSG_ON = """
-🔥 **PociUserbot Berhasil Di Aktifkan**
+‼️ **PociUserbot Berhasil Di Aktifkan**
 
-┌•**Userbot Version -** `{}`
-└•**Ketik** `{}alive` **untuk Mengecheck Bot**
+**┌ [ Userbot Version -** `{}` 
+**├ [ Ketik** `{}alive` **untuk Mengecek bot**
+**└ [ Harap Gunakan PociUserbot ini dengan bijak!!**
 """
 
 
 async def main():
+    for all_module in ALL_MODULES:
+        importlib.import_module(f"PociUb.modules.{all_module}")
     for bot in bots:
         try:
             await bot.start()
@@ -41,7 +47,7 @@ async def main():
             )
         except Exception as a:
             LOGGER("main").warning(a)
-    LOGGER("PociUb").info(f"PociUserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    LOGGER("PociUb").info(f"PociUb v{BOT_VER} [🔥 - BERHASIL DIAKTIFKAN! - 🔥]")
     if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
         await create_botlog(bot1)
     await idle()
@@ -51,6 +57,5 @@ async def main():
 if __name__ == "__main__":
     LOGGER("PociUb").info("Starting PociUserBot")
     install()
-    git()
     heroku()
     LOOP.run_until_complete(main())
